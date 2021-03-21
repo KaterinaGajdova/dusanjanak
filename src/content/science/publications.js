70e6d6289b892ab5d062b10abd37a6a5c412692e
LenkaH <hubackova.lenka@gmail.com>
2021-03-21 21:01:31 +0100

    publications update

--- src/content/science/publications.js
+++ src/content/science/publications.js
@@ -13,15 +13,24 @@ export const Cz = () => {
   return (
     <Content>
       <Section>
-        <div>Knihy: 8 (3 jediný autor, 4 hlavní autor, 1 spolblikuautor)</div>
-        <div>Články v časopisech s impakt faktorem: 12</div>
+        <div>Knihy: 9 (3 jediný autor, 5 hlavní autor, 1 spoluautor)</div>
+        <div>Články v časopisech indexovaných na Web of Science: 12</div>
         <div>Články v časopisech indexovaných ve SCOPUS: 5</div>
         <div>Články v ostatních vědeckých časopisech: 10</div>
-        <div> Slovníková hesla: 18</div>
-        <div>Kapitoly v knihách: 14</div>
+        <div>Kapitoly v knihách: 17</div>
+        <div>Slovníková hesla: 18</div>
       </Section>
       <Section>
         <H4>Knihy, odborné monografie</H4>
+        <Project>
+          Janák, D. a kolektiv. 2020.
+          <i>
+            Jídlo není jenom &quot;něco k jídlu&quot;: Tradiční stravování a
+            pokrmy jako součást regionální a národní identity ve vědomí a paměti
+            současné společnosti.
+          </i>
+          Opava: Slezská univerzita. ISBN 978-80-7510-428-1.
+        </Project>
         <Project>
           Janák, D. 2018.
           <i>
@@ -83,14 +92,7 @@ export const Cz = () => {
       </Section>
 
       <Section>
-        <H4>
-          Odborné články a statě v odborných časopisech (reviewed artcicles)
-        </H4>
-        <Project>
-          Janák, D. 2020. (V TISKU) Kritický realismus <i>revisited</i>:
-          Příspěvek k sociologické koncepci subjektivity. <i>Sociální studia</i>
-          , 17 (1)
-        </Project>
+        <H4>Články v časopisech indexovaných na Web of Science:</H4>
 
         <Project>
           Janák, D. 2019. „Problematika národa v počátcích sociologické teorie
@@ -98,6 +100,11 @@ export const Cz = () => {
           Gellnera: Příspěvek k historické sociologii vědění.“{" "}
           <i>Sociológia - Slovak Sociological Review.</i> Vol. 51, no. 2 (2019),
           p. 152-178. DOI: https://doi.org/10.31577/sociologia.2019.51.2.8.
+          <br />
+          Dostupné{" "}
+          <a href="https://www.sav.sk/journals/uploads/04231017Janak%202-2019.pdf">
+            zde
+          </a>
         </Project>
         <Project>
           Janák, D. 2018. Tři tradice sociologie venkova v počátcích sociologie
@@ -105,40 +112,7 @@ export const Cz = () => {
           <i>Sociológia - Slovak Sociological Review.</i> Vol. 50, no. 2 (2018),
           p. 149-171. ISSN 0049-1225, ISSN 1336-8613.
         </Project>
-        <Project>
-          Janák, D. 2016. „The Nation and the Nation State in the Mirror of
-          Czech and Polish Sociological Journals in the Interwar Era“{" "}
-          <i>Central European Papers</i>, 4 (2): 8 – 24.
-        </Project>
-        <Project>
-          Janák, D., Kuděla M., Kulhavý V. 2016. „Sociologický výzkum města Brna
-          z roku 1947: Analýza datového souboru podle základních
-          sociodemografických charakteristik.“ <i>Brno v minulosti a dnes</i>{" "}
-          29: 330 – 356.
-        </Project>
-        <Project>
-          Martin Stanoev, Dušan Janák, Kateřina Tvrdá. 2016. „Social
-          disadvantage: Challenge for educational sciences and educators.“
-          <i>Social Pathology and Prevention.</i> 2 (1): 11-35.
-        </Project>
-        <Project>
-          Janák, D., J. Bezděk, V. Walach. 2015.„Social exclusion under the
-          auspices of the state? Case study of social exclusion, ethnicity and
-          migration in Osoblaha Region.“ <i>Central European Papers</i> 3 (1):
-          117- 140, ISSN 2336-3312 (Print), ISSN 2336-369X (Online).
-        </Project>
-        <Project>
-          Janák, D. 2014. „Czech Sociology of Industrial Working Class until
-          1948.“ <i>Central European Papers 2</i> (1): 115 – 131, ISSN 2336-3312
-          (Print), ISSN 2336-369X (Online).
-        </Project>
-        <Project>
-          Janák, Dušan, Martin Stanoev a Petr Hušek. 2014. „Poválečný
-          sociologický výzkum města Brna z roku 1947 a metodika jeho soudobého
-          zpracování z pohledu historicko-sociologické analýzy průmyslového
-          dělnictva před rokem 1948.“ <i>Data a výzkum – SDA Info</i> 127 (2):
-          143-163, http://dx.doi.org/10.13060/23362391.2014.127.2.157
-        </Project>
+
         <Project>
           Janák, Dušan, Robert Klobucký. 2014. „Co bychom věděli o sociologii,
           kdybychom četli pouze Sociologický časopis a Sociológii? Obsahová
@@ -150,15 +124,7 @@ export const Cz = () => {
           Janák, D. 2014. „Problém národní identity v díle Edvarda Beneše.“
           <i>Sociologický časopis.</i> roč. 50 (2): 233-263. ISSN 0038-0288.
         </Project>
-        <Project>
-          Janák, D. - Klobucký, R. 2014. “The issue of Central Europe in the
-          main Czech and Slovak sociological journals{" "}
-          <i>Sociologický časopis/Czech Sociological Review</i> and{" "}
-          <i>Sociológia/Slovak Sociological Review</i> between 1990 and 2000.”{" "}
-          <i>Socio.hu</i> 4 (2): 74-95 dostupný z:
-          http://socio.hu/uploads/files/Visegrad/5janak_klobucky.pdf (cit.
-          on-line 26.5. 14) (ISSN 2063-0468)
-        </Project>
+
         <Project>
           Janák, D. 2013. „Počátky teoretické sociologie v českých zemích. Text
           věnovaný památce prof. M. Petruska.“
@@ -187,16 +153,34 @@ export const Cz = () => {
           <i>Sociológia - Slovak Sociological Review</i> 43 (5): 584-603, ISSN
           0049-1225, ISSN 1336- 8613.
         </Project>
-        <Project>
-          Janák D. - Bereś A. 2011. „Autorská a tematická struktura časopisu
-          <i>Sociální problémy</i>. Příspěvek k sociologickému rozboru české
-          sociologie.“ <i>Lidé města</i> 13 (1): 3-25, ISSN 1212-8112.
-        </Project>
+
         <Project>
           Janák, D. 2011. „Autorská a tematická struktura{" "}
           <i>Sociologické revue</i>. Příspěvek k sociologickému rozboru dějin
           české sociologie.“
           <i>Sociologický časopis</i> 47 (5): 991-1016, ISSN 0038-0288.
+        </Project>
+
+        <Project>
+          Janák, D. 2009. „Ohlédnutí za sociologií Inocence Arnošta Bláhy.“
+          <i>Sociologický časopis</i> roč. 45, č. 5, s. 1081-1090, ISSN
+          0038-0288.
+        </Project>
+
+        <Project>
+          Janák, D. 2006. „Sociální introspekce In. A. Bláhy a Wittgensteinův
+          argument proti soukromému jazyku. Poznámky k introspektivnímu přístupu
+          v sociálních vědách.” <i>Sociologický časopis</i>, Vol. 42 (No.4): 761
+          – 782, ISSN:0038-0288.
+        </Project>
+      </Section>
+
+      <Section>
+        <H4>Články v časopisech indexovaných na Web of Science:</H4>
+        <Project>
+          Janák, D. 2020. &quot;Kritický realismus <i>revisited</i>: Příspěvek k
+          sociologické koncepci subjektivity.&quot; <i>Sociální studia</i>, 17
+          (1): 69-88. ISSN: 1803-6104 (on-line), ISSN: 1214-813X (print).
         </Project>
         <Project>
           Janák, D. 2010.&quot;Social Introspection Of I. A. Bláha and
@@ -204,6 +188,72 @@ export const Cz = () => {
           Study On Introspection Approach In Social Sciences.&quot;{" "}
           <i>Qualitative Sociology Review</i>, roč.6 č. 2, s. 51-72, ISSN
           1733-8077.
+        </Project>
+        <Project>
+          Janák D. - Buchtíková O. 2008. „Sociologie města a filmu. Inocenc
+          Arnošt Bláha a jeho role ve výzkumu Brna po druhé světové válce.“
+          <i>Iluminace</i> Vol. 20, No.1 (69): 161-178, ISSN: 0862-397X.
+        </Project>
+        <Project>
+          Janák, D. 2006. „In. A. Bláha a počátky české sociologie města“
+          <i>Sociální studia</i> 2/2006: 11-32, ISSN: 1214-813X.
+        </Project>
+        <Project>
+          Janák, D. 2005. „Sociální věda versus přírodní věda. Opozice přírody a
+          kultury v klasickém sporu o metodu sociálních věd a jeho brněnské
+          reminiscence.” <i>Sociální studia</i> 2/2005:71-87, ISSN: 1214- 813X.
+        </Project>
+      </Section>
+
+      <Section>
+        <H4>Studie a články v ostatních vědeckých časopisech:</H4>
+        <Project>
+          Janák, D., Kuděla M., Kulhavý V. 2016. „Sociologický výzkum města Brna
+          z roku 1947: Analýza datového souboru podle základních
+          sociodemografických charakteristik.“ <i>Brno v minulosti a dnes</i>{" "}
+          29: 330 – 356.
+        </Project>
+        <Project>
+          Martin Stanoev, Dušan Janák, Kateřina Tvrdá. 2016. „Social
+          disadvantage: Challenge for educational sciences and educators.“
+          <i>Social Pathology and Prevention.</i> 2 (1): 11-35.
+        </Project>
+        <Project>
+          Janák, D. 2016. „The Nation and the Nation State in the Mirror of
+          Czech and Polish Sociological Journals in the Interwar Era“{" "}
+          <i>Central European Papers</i>, 4 (2): 8 – 24.
+        </Project>
+        <Project>
+          Janák, D., J. Bezděk, V. Walach. 2015.„Social exclusion under the
+          auspices of the state? Case study of social exclusion, ethnicity and
+          migration in Osoblaha Region.“ <i>Central European Papers</i> 3 (1):
+          117- 140, ISSN 2336-3312 (Print), ISSN 2336-369X (Online).
+        </Project>
+        <Project>
+          Janák, D. 2014. „Czech Sociology of Industrial Working Class until
+          1948.“ <i>Central European Papers 2</i> (1): 115 – 131, ISSN 2336-3312
+          (Print), ISSN 2336-369X (Online).
+        </Project>
+        <Project>
+          Janák, Dušan, Martin Stanoev a Petr Hušek. 2014. „Poválečný
+          sociologický výzkum města Brna z roku 1947 a metodika jeho soudobého
+          zpracování z pohledu historicko-sociologické analýzy průmyslového
+          dělnictva před rokem 1948.“ <i>Data a výzkum – SDA Info</i> 127 (2):
+          143-163, http://dx.doi.org/10.13060/23362391.2014.127.2.157
+        </Project>
+        <Project>
+          Janák, D. - Klobucký, R. 2014. “The issue of Central Europe in the
+          main Czech and Slovak sociological journals{" "}
+          <i>Sociologický časopis/Czech Sociological Review</i> and{" "}
+          <i>Sociológia/Slovak Sociological Review</i> between 1990 and 2000.”{" "}
+          <i>Socio.hu</i> 4 (2): 74-95 dostupný z:
+          http://socio.hu/uploads/files/Visegrad/5janak_klobucky.pdf (cit.
+          on-line 26.5. 14) (ISSN 2063-0468)
+        </Project>
+        <Project>
+          Janák D. - Bereś A. 2011. „Autorská a tematická struktura časopisu
+          <i>Sociální problémy</i>. Příspěvek k sociologickému rozboru české
+          sociologie.“ <i>Lidé města</i> 13 (1): 3-25, ISSN 1212-8112.
         </Project>
         <Project>
           Janák, D. 2010. „Klasická sociologie Inocence A. Bláhy v kontextu
@@ -215,31 +265,6 @@ export const Cz = () => {
           społeczeństwem lat trzydziestych XX wieku. (Na podstawie analizy
           zawartości „Przeglądu socjologicznego“ i „Sociologická revue“)“{" "}
           <i>Studia Śląskie</i>, roč. LXIX, s.269-285, ISSN 0039-3355.
-        </Project>
-        <Project>
-          Janák, D. 2009. „Ohlédnutí za sociologií Inocence Arnošta Bláhy.“
-          <i>Sociologický časopis</i> roč. 45, č. 5, s. 1081-1090, ISSN
-          0038-0288.
-        </Project>
-        <Project>
-          Janák D. - Buchtíková O. 2008. „Sociologie města a filmu. Inocenc
-          Arnošt Bláha a jeho role ve výzkumu Brna po druhé světové válce.“
-          <i>Iluminace</i> Vol. 20, No.1 (69): 161-178, ISSN: 0862-397X.
-        </Project>
-        <Project>
-          Janák, D. 2006. „Sociální introspekce In. A. Bláhy a Wittgensteinův
-          argument proti soukromému jazyku. Poznámky k introspektivnímu přístupu
-          v sociálních vědách.” <i>Sociologický časopis</i>, Vol. 42 (No.4): 761
-          – 782, ISSN:0038-0288.
-        </Project>
-        <Project>
-          Janák, D. 2006. „In. A. Bláha a počátky české sociologie města“
-          <i>Sociální studia</i> 2/2006: 11-32, ISSN: 1214-813X.
-        </Project>
-        <Project>
-          Janák, D. 2005. „Sociální věda versus přírodní věda. Opozice přírody a
-          kultury v klasickém sporu o metodu sociálních věd a jeho brněnské
-          reminiscence.” <i>Sociální studia</i> 2/2005:71-87, ISSN: 1214- 813X.
         </Project>
       </Section>
     </Content>
