a4033dbbcb67ab657153ad889e81fa38bcc99626
LenkaH <hubackova.lenka@gmail.com>
2021-03-29 23:09:54 +0200

    awards update

--- src/content/alpinism/awards.js
+++ src/content/alpinism/awards.js
@@ -47,8 +47,204 @@ export const Cz = () => {
             srovnatelný se světovou špičkou a Čestné uznání za výkon vynikající
             v českém kontextu.
           </p>
-          IN PROGRESS...
+          <Project>
+            <Date>2015</Date>
+            <Name>
+              VÝSTUP ROKU V KATEGORII VELEHORY POD 6000 m.n.m. SKALNÍ LEZENÍ A
+              BIG WALL:
+            </Name>
+            Bellavista - Jan Zbranek, Dušan Janák; 8c (8b+), přelezení cesty A.
+            Hubera, 500 m, nejtěžší délka vylezena stylem PP, Dolomity, Cima
+            Ovest; září 2015
+          </Project>
+          <Project>
+            <Date>2014</Date>
+            <Name>
+              ČESTNÉ UZNÁNÍ V KATEGORII HORY POD 6000 m.n.m. MIXOVÝ A LEDOVCOVÝ
+              CHARAKTER:
+            </Name>
+            No Siesta přes Bonnatti Voucher - Pavel Vrtík a Dušan Janák; Grandes
+            Jorasses, 1200 m mixového lezení do M7, OS; září 2014
+          </Project>
+          <Project>
+            <Date>2013</Date>
+            <Name>
+              ČESTNÉ UZNÁNÍ V KATEGORII VELEHORY POD 6000 m.n.m. SKALNÍ LEZENÍ A
+              BIG WALL:
+            </Name>
+            Dušan Janák, Václav Šatava, Miro Peťo – Dionýsos Alpy, oblast Mont
+            Blanc, Petit Jorasses; prvovýstup Z stěnou; 7 b/c fr., délka výstupu
+            600 m, 18 délek; 14.- 16.8. 2013.
+          </Project>
+          <Project>
+            <Date>2011</Date>
+            <Name>
+              VÝSTUP ROKU V KATEGORII HORY POD 6000 m.n.m. MIXOVÝ A LEDOVCOVÝ
+              CHARAKTER:
+            </Name>
+            Dušan Janák, Pavel Vrtík (oba HO Lokomotiva Brno),Tatranský double
+            na Ganek – první volné přelezení Centrálního koutu na galerii Ganku
+            ve Vysokých Tatrách klas. 8 UIAA, M8-, (Galerie Ganku a výstup na
+            Velký Ganek, VI+), 9.-12.3. 2011
+            <br />
+            <br />
+            <Name>
+              ČESTNÉ UZNÁNÍ V KATEGORII VELEHORY POD 6000 m.n.m. SKALNÍ LEZENÍ A
+              BIG WALL:
+            </Name>
+            Dušan Janák a Miro Peťo – Erectissima, první kontinuální volný
+            přelez, 8a/8a+
+          </Project>
+          <Project>
+            <Date>2010</Date>
+            <Name>
+              VÝSTUP ROKU V KATEGORII HORY POD 6000 m.n.m. MIXOVÝ A LEDOVCOVÝ
+              CHARAKTER:
+            </Name>
+            Dušan Janák, Pavel Vrtík – přelez cesty Vol de Nuit – Francie,
+            oblast Mont Blanc, Mont Blanc du Tacul (4248 m n.m.) M8-, 450 m,
+            stylem OS Opakování cesty Roberta Jaspera z r. 1998 a pravděpodobně
+            první český přelez. Cesta je označována za jednu z nejtěžších
+            mixových cest v Alpách, byla zopakována v dosti náročných
+            podmínkách, s malým množstvím ledu a při teplotách pod -25°C. Leden
+            2010
+            <br />
+            <br />
+            <Name>
+              ČESTNÉ UZNÁNÍ V KATEGORII HORY POD 6000 m.n.m. SKALNÍ CHARAKTER:
+            </Name>
+            Pavel Jonák, Václav Šatava, Dušan Janák, Richard Jurečka –
+            prvovýstup Erectissima + Supererectissima – Itálie, Dolomity, Tre
+            Cime di Lavaredo, Cima Grande 8a+fr. (10-UIAA; nejtěžší 2.délka 8a+;
+            obtížnosti 8-, 10-, 9-, 9-, 9, 7+/8-, 7+, 7, 9, 9-, 8/8+, 7+/8-,
+            7,7-, 7, 8-, 5+), 17 délek, 560 m prvovýstup - červenec 2009, první
+            volný přelez jednotlivých délek - červenec 2010
+          </Project>
+          <Project>
+            <Date>2009</Date>
+            <Name>
+              ČESTNÉ UZNÁNÍ V KATEGORII MIXY A LEDY SPORTOVNÍHO CHARAKTERU:
+            </Name>
+            Dušan Janák, Richard Jurečka – Hazard - Vyskoké Tatry, severní stěna
+            Velkého kostola 8 / A5, délka cca 250 m první opakování nejtěžší
+            technické cesty v Tatrách
+          </Project>
+          <Project>
+            <Date>2008</Date>
+            <Name>
+              VÝSTUP ROKU V KATEGORII HORY POD 6000 m.n.m. MIXOVÝ A LEDOVCOVÝ
+              CHARAKTER:
+            </Name>
+            Vysoké Tatry, severní stěna Malého Kežmarského štítu, cesta Matrix
+            M9-, Dušan Janák, Pavel Vrtík.
+          </Project>
+          <Project>
+            <Date>2007</Date>
+            <Name>
+              VÝSTUP ROKU V KATEGORII VELEHORY POD 6000 m.n.m. SKALNÍ
+              ALPINISTICKÝ CHARAKTER:
+            </Name>
+            Francie, oblast Mt. Blanc, Grand Capucin, Voie Petit, 8b, E3, 2.
+            volný přelez Václav Šatava, Dušan Janák, 26. 8. 2007. Po úspěchu A.
+            Hubera v roce 2005 je to pravděpodobně jediný další (tj. druhý)
+            volný přelez. Jedná se o 14 délek spár, ploten a převisů cca 450 m).
+            Ačkoli jsou v cestě použity nýty, převážná část je jištěna vklíněnci
+            a jejich použití je nezbytné i v nejtěžších délkách. Při lezení se
+            oba ve vedení jednotlivých délek střídali, druholezec jumaroval.
+            <br />
+            <br />
+            <Name>
+              VÝSTUP ROKU V KATEGORII VELEHORY POD 6000 m.n.m. SKALNÍ
+              ALPINISTICKÝ CHARAKTER:
+            </Name>
+            Vysoké Tatry, Jastrabia veža, JV stěna, 200 m, Jet Stream, 10-/10
+            UIAA, E4, styl PP. Dušan Janák, 24. 9. 2007 1. volné přelezení
+            stupně 10-/10 UIAA E4 („E“ v Jasperově šestimístné stupnici
+            vyjadřuje psychickou náročnost a nebezpečnost výstupu), nejtěžší
+            délky byly přelezeny stylem PP. V Jet Streamu jsou celkem 3
+            postupové nýty, jinak je cesta zajištěna coperheady, rurpy a
+            vklíněnci. Volným přelezem se podařilo posunout hranici volného
+            lezení v Tatrách v pískařských intencích – vyrovnanosti fyzické a
+            psychické náročnosti cesty. Jiné těžké cesty (dosahující v Polsku
+            stupně 10-, na Slovensku 9+) jsou vesměs sportovního charakteru,
+            tedy vynýtované, bez větší psychické náročnosti.
+          </Project>
+          <Project>
+            <Date>2006</Date>
+            <Name>
+              VÝSTUP ROKU V KATEGORII HORY POD 6000 m.n.m. SKALNÍ CHARAKTER:
+            </Name>
+            Dušan Janák, Václav Šatava, Dolomity, Marmolada, Fram: End of
+            Mystery, první RP přelez
+            <br />
+            <br />
+            <Name>
+              ČESTNÉ UZNÁNÍ V KATEGORII VELEHORY DO 6000 m.n.m. SKALNÍ LEZENÍ A
+              BIG WALL:
+            </Name>
+            Dušan Janák, Jiří Šrůtek, Josemity, El Capitan, Free Rider 5.12d ,
+            38 délek
+          </Project>
+
+          <Project>
+            <Date>2005</Date>
+            <Name>
+              ČESTNÉ UZNÁNÍ V KATEGORII MIXY A LEDY SPORTOVNÍHO CHARAKTERU:
+            </Name>
+            Gastein (Rakousko) Rodeo WI7,M8+/9-,E5,AF (pův.M8,A2) 1.čistý přelez
+            Jan Doudlebský, Dušan Janák
+            <br />
+            <br />
+            <Name>ZVLÁŠTNÍ OCENĚNÍ:</Name>
+            Vysoké Tatry, Jastrabia veža, „Jet Stream“ - 1.čistý přelez 9+,
+            E4/5(AF), bez přidání stálého jištění (původně 6+A3+) Dušan Janák a
+            Jan Kreisinger
+            <br />
+            <br />
+            <Name>ZVLÁŠTNÍ OCENĚNÍ:</Name>
+            Lofoty,Vogokollen,Freya E5 (9 UIAA,A3+) Dušan Janák a Jan
+            Kreisinger, červen 2005, 1.opakování
+            <br />
+            <br /> <Name>ZVLÁŠTNÍ OCENĚNÍ:</Name>
+            Dolomity, Marmolada -„End of Mystery“ 9+ AF, E4 Pavel Jonák, Václav
+            Šatava, Dušan Janák , srpen 2005 (dokončení cesty „Fram“ z Ryby do
+            police ve středu stěny, lezeno od spodu)
+          </Project>
+
+          <Project>
+            <Date>2004</Date>
+            <Name>
+              VÝSTUP ROKU V KATEGORII HORY POD 6000 m.n.m. SKALNÍ CHARAKTER:
+            </Name>
+            Cima Ovest, Dolomity – „Na památku Jeany Couzyho“ 8b (8a+) D. Janák,
+            J. Doudlebský „Výstup roku 2004“ <br />
+            <br />
+            <Name>
+              ČESTNÉ UZNÁNÍ V KATEGORII HORY POD 6000 m.n.m. SKALNÍ CHARAKTER:
+            </Name>
+            Cima Grande, Dolomity – cesta Camilotto Pellesier 8b (8a/a+)
+            D.Janák, S. Hovanec
+            <p>
+              - Komise dále vyhodnotila jako vynikající také následující
+              výstupy: Dolomity, Tre Cime – „Via Robinson Crusoe“ - prvovýstup
+              8+/9-, napojení do cesty „Alpská růže“ D.Janák, J. Doudlebský
+            </p>
+          </Project>
+          <Project>
+            <Date>2003</Date>
+            <Name>
+              ČESTNÉ UZNÁNÍ V KATEGORII STŘEDNÍ HORY - SPORTOVNÍ CHARAKTER:
+            </Name>
+            Cima Ovest - " AKUT" (skupina Tre Cime, Dolomity, Itálie) kl. 10-
+            UIAA, 15. 7. 2003, vylezeno stylem RP team kromě 2 délek Dušan
+            Janák, Stanislav Hovanec těžká cesta vylezená nastupující lezeckou
+            generací v dobrém stylu (RP s výjimkou 4. délky PP a 5. délky RK).
+            Cesta od Kurta Astnera vede v pravé části Cimy Ovest mezi Střechou
+            světa a Šepotem květin od T. Grosse. Prvních 300m cesty je
+            převislých, obtíže kulminují na 10. délce obtížností X-.
+          </Project>
         </Section>
+
         <Section>
           <H4>Horolezec roku</H4>
           <Project>
@@ -68,8 +264,7 @@ export const Cz = () => {
             (za přelez cesty Vol de Nuit – Francie, oblast Mont Blanc, Mont
             Blanc du Tacul (4248 m n.m.)
           </Project>
-        </Section>
-        <Section>
+          <br />
           <H4>Závody</H4>
           <Project>
             <Date>2006</Date> <br />
@@ -91,7 +286,13 @@ const Main = styled.div`
   display: grid;
 
   @media (min-width: 1024px) {
-    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-columns: 2fr 1fr;
+    > section {
+      min-width: 100%;
+      &:first-of-type {
+        padding-right: 5vw;
+      }
+    }
   }
 `;
 
