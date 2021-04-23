4efc190a3c85b92da242e86257961719e85f680c
LenkaH <hubackova.lenka@gmail.com>
2021-04-23 22:43:27 +0200

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,14 @@
 import React from "react";
 import SEO from "../components/seo";
 import styled from "styled-components";
+import { Link } from "gatsby";
 
 const NotFoundPage = () => (
   <MissingWrapper>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Link to="/"> Try your luck here</Link>
   </MissingWrapper>
 );
 
@@ -14,6 +16,7 @@ export default NotFoundPage;
 
 const MissingWrapper = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   text-align: center;
